6264a4958d1785bb740f3c368d91557dca0b5cde
Leah Kim <lk.leahkim@gmail.com>
2022-04-14 22:09:02 -0700

    small changes business prof page

--- frontend/src/components/OneBusinessPage/index.js
+++ frontend/src/components/OneBusinessPage/index.js
@@ -45,7 +45,7 @@ function OneBusinessPage() {
         <div>
             {specificBusiness.map(business => (
                 <div key={business.id}>
-                    {business.title}
+                        <p className="one-biz-title">{business.title}</p>
                     <div className="one-biz-pic-container">
                         <img className="one-biz-photo"src={business.imageUrl} alt="one-biz-pic"/>
                         <img className="one-biz-photo"src={business.imageUrlTwo} alt="one-biz-pic"/>
@@ -56,7 +56,7 @@ function OneBusinessPage() {
                             <button onClick={goToBusinessEditPage}>Update Business</button>
                             <button onClick={deleteBusiness}>Delete Business</button>
                         </div>
-                    : <p>hello</p>}
+                    : ''}
                 </div>
             ))}
 
