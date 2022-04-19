3c92b12482469a5b1a4ab561404ab270464df042
Leah Kim <lk.leahkim@gmail.com>
2022-04-18 19:23:20 -0700

    comitting before i break anything

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -7,6 +7,7 @@ import shelfIcon from '../IconPics/shelf.png';
 // import magnify from '../IconPics/mag.png';
 
 function Navigation({ isLoaded }){
+  const pathName = window.location.pathname;
   const sessionUser = useSelector(state => state.session.user);
   const history = useHistory();
 
@@ -28,11 +29,13 @@ function Navigation({ isLoaded }){
     <div className="navbar-container">
       <NavLink className="navbar-links" exact to="/"> <img src={shelfIcon} alt="shelf-icon"/></NavLink>
 
-    <div className="search-container">
-      <input className="search-input"
-      type="text"
-      />
-    </div>
+    {pathName !== '/' ?
+      <div className="search-container">
+        <input className="search-input"
+        type="text"
+        />
+      </div>
+    : ''}
 
       <div className="main-nav-links">
         {sessionUser ?
