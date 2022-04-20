53b2fe0f7f75ebbe3e76f4125833e68ebe50f720
Leah Kim <lk.leahkim@gmail.com>
2022-04-19 22:24:13 -0700

    seeded some

--- backend/db/seeders/20201030163017-demo-user.js
+++ backend/db/seeders/20201030163017-demo-user.js
@@ -6,30 +6,27 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-        firstName: 'Demo User',
-        lastName: 'Trial',
-        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/main/frontend/src/components/IconPics/biz-owner-pic.jpg?raw=true',
+        firstName: 'Demo',
+        lastName: 'User',
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/eating-bear.jpg?raw=true',
         city: 'App Academy',
         email: 'demo@aa.io',
-        // username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password'),
       },
       {
         firstName: 'Leah',
         lastName: 'Kim',
-        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/main/frontend/src/components/IconPics/bread-cat.jpg?raw=true',
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/white-bear.jpg?raw=true',
         city: 'San Jose',
         email: 'leah@aa.io',
-        // username: 'Leahk1m',
         hashedPassword: bcrypt.hashSync('password'),
       },
       {
         firstName: 'John',
         lastName: 'Angcla',
-        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/main/frontend/src/components/IconPics/eating-bear.jpg?raw=true',
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/brown-bear.jpg?raw=true',
         city: 'Bay Point',
         email: 'john@aa.io',
-        // username: 'shinobu',
         hashedPassword: bcrypt.hashSync('password'),
       },
     ], {});
