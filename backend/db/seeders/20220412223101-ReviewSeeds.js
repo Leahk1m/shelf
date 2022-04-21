6c8b8c44253daa0df77445629d59136b64d9f6a8
Leah Kim <lk.leahkim@gmail.com>
2022-04-20 22:48:51 -0700

    seeded all reviews yay

--- backend/db/seeders/20220412223101-ReviewSeeds.js
+++ backend/db/seeders/20220412223101-ReviewSeeds.js
@@ -168,9 +168,153 @@ module.exports = {
         rating: 5,
         post: "I wish I could go to Yuji for all of my grocery needs because they have it all. The modern interior is beautiful and the food is as fresh as it could get. I just wish that the prices were a bit more affordable but I guess it makes sense, since it's located in Walnut Creek. "
       },
-      
+      {
+        firstName: 'Kevin',
+        lastName: 'Chao',
+        userId: 7,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/kevin.jpg?raw=true',
+        businessId: 10,
+        rating: 2,
+        post: 'I thought this place was based off the dude from jujutsu kaisen. Booooo.'
 
+      },
+      {
+        firstName: 'Michelle',
+        lastName: 'Meng',
+        userId: 4,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/michelle.png?raw=true',
+        businessId: 10,
+        rating: 5,
+        post: "I don't understand why some people leave stupid reviews.... Anyways, Yuji is beautifully decorated and honestly exceeded my expectations with the quality of their products."
+      },
+      {
+        firstName: 'Roxanna',
+        lastName: 'Zuniga',
+        userId: 6,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/roxxie.jpg?raw=true',
+        businessId: 11,
+        rating: 5,
+        post: "This place blew my mind! I never knew I could be shopping for groceries at such a beautiful location. It felt like I was shopping for my dinner at a high end museum. I will be back every weekend for sure!"
+      },
+      {
+        firstName: 'Jordan',
+        lastName: 'Anderson',
+        userId: 5,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/jordy.jpg?raw=true',
+        businessId: 11,
+        rating: 5,
+        post: "I found this place by accident when I was walking through the city and I didn't have that much time to explore it but it was so beautiful I look forward to going back."
+      },
+      {
+        firstName: 'John',
+        lastName: 'Angcla',
+        userId: 3,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/brown-bear.jpg?raw=true',
+        businessId: 12,
+        rating: 4,
+        post: "I thought this place was for kids but my girlfriend talked me into going with her and I honestly had so much fun. The ice cream they sell here was so good I wish they sold it at stores closer to me. This place would have been 5 stars but I took one off because I kept finding sprinkles in bad places..."
+      },
+      {
+        firstName: 'Leah',
+        lastName: 'Kim',
+        userId: 2,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/white-bear.jpg?raw=true',
+        businessId: 12,
+        rating: 5,
+        post: "The ice cream really is delicious here! You should definitely try their cherry garcia flavor if you ever get a chance to visit."
+      },
+      {
+        firstName: 'Jordan',
+        lastName: 'Anderson',
+        userId: 5,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/jordy.jpg?raw=true',
+        businessId: 13,
+        rating: 5,
+        post: "This is a small convenience store, but with a modern twist. Everything was spotless and they had a coffee shop that served one of the best affogatos I've ever had! This place is a hidden gem."
+      },
+      {
+        firstName: 'Roxanna',
+        lastName: 'Zuniga',
+        userId: 6,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/roxxie.jpg?raw=true',
+        businessId: 14,
+        rating: 5,
+        post: "This is a very small store, although it doesn't look like it from the outside. The decor is not my cup of tea but prices were very reasonable and their produce is organic and very fresh. I think it's a great location in Palo Alto."
+      },
+      {
+        firstName: 'Michelle',
+        lastName: 'Meng',
+        userId: 4,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/michelle.png?raw=true',
+        businessId: 14,
+        rating: 4,
+        post: "There aren't many artisanal shops in Palo Alto, so the Goods Mart really stands out as one of the best places to visit for some great quality grocery items for sure."
+      },
+      {
+        firstName: 'John',
+        lastName: 'Angcla',
+        userId: 3,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/brown-bear.jpg?raw=true',
+        businessId: 15,
+        rating: 5,
+        post: "It's hard to believe that this is a grocery store because the design of it all is so futuristic. I only stopped by to grab a quick drink, but they had such a huge selection of everything. I definitely plan on coming back to explore more of it."
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Chao',
+        userId: 7,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/kevin.jpg?raw=true',
+        businessId: 15,
+        rating: 3,
+        post: "It's no Omega Mart but this place was cool too. I liked their neon lights so I would probably choose this place over like, wal-mart."
+      },
+      {
+        firstName: 'Michelle',
+        lastName: 'Meng',
+        userId: 4,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/michelle.png?raw=true',
+        businessId: 16,
+        rating: 5,
+        post: "I love Eataly so much, it reminds me of home! Not Italy, but New York, since I used to go to the NY location all the time. I'm so glad the bay's getting nicer things."
 
+      },
+      {
+        firstName: 'Leah',
+        lastName: 'Kim',
+        userId: 2,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/white-bear.jpg?raw=true',
+        businessId: 16,
+        rating: 5,
+        post: "My best friend showed me the ways of Eataly and it's honestly a whole experience that can't be rated on food alone. Although, the food was amazing too. "
+      },
+      {
+        firstName: 'Roxanna',
+        lastName: 'Zuniga',
+        userId: 6,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/roxxie.jpg?raw=true',
+        businessId: 17,
+        rating: 5,
+        post: "I thought it was a gas station from the outside, so I was really surprised when I went in to pay for my gas. I didn't really have time to shop but the concept of it all is so intriguing. I will be back with my sister next weekend, since she loves interesting places like these."
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Chao',
+        userId: 7,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/kevin.jpg?raw=true',
+        businessId: 17,
+        rating: 2,
+        post: "This isn't a gas station??? My car stopped because I used up my remaining 5 miles to get here and they didn't have gas but it's whatever."
+      },
+      {
+        firstName: 'Demo',
+        lastName: 'User',
+        userId: 1,
+        profilePhoto: 'https://github.com/Leahk1m/shelf_app/blob/styling_stuff/frontend/src/components/IconPics/eating-bear.jpg?raw=true',
+        businessId: 18,
+        rating: 5,
+        post: 'They have a pasta dispenser!! That is so cool, I love it.'
+
+      }
 
     ], {});
     /**
