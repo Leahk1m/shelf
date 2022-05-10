a7e78e667462f08ebd69ec3eff613b3f11e201ac
Leah Kim <lk.leahkim@gmail.com>
2022-05-10 15:05:59 -0700

    added contact me and portfolio to footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,17 +2,22 @@ import React from "react";
 import './Footer.css';
 import { FaGithub } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 function Footer() {
     return(
         <div className="footer-div">
             <div className="about-div">
                 <h3>About</h3>
-                {/* <p>shelf is a clone of Yelp, in which users can explore and rate businesses that center around artisanal grocery stores. It is meant for those who love shopping at unconventional places, made easy, with eccentric locations all listed in one spot.  </p> */}
+                <a className="portfolio-link-footer"target="_blank" href="http://www.leahkim.net/">Portfolio</a>
+                <Link className="portfolio-link-footer"onClick={() => window.location = 'mailto:lk.leahkim@gmail.com'}>Contact Me</Link>
+
+
 
             </div>
 
             <div className="discover-div">
+                <h3>Discover</h3>
                 <div className="github-div">
                     <FaGithub className="github-logo"/><a target="_blank"className="github-link"href='https://github.com/Leahk1m'>Github</a>
                 </div>
